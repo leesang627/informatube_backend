6c89c76a571144544140f08f0df9a36dbc8c8b5a
Sangwook Lee <leesang627@gmail.com>
2020-01-10 15:08:58 +0900

    informatube-backend commit

--- routes/infostamp.js
+++ routes/infostamp.js
@@ -24,16 +24,14 @@ router.post('/url', function(req, res, next) {
     } else {
       capturedUrl = 'http://'+url;
     }
-    const browser = await puppeteer.launch({
-      defaultViewport: {
-        width: 400,
-        height: 860,
-        deviceScaleFactor: 1.5,
-      }
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1');
+    await page.setUserAgent('Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36');
     await page.goto(capturedUrl, {waitUntil: 'networkidle2'});
+    await page.setViewport({
+      width: 399,
+      height: 860,
+    });
     await page.evaluate(() => {
       let elements = document.getElementsByClassName('collapsible-heading');
       for (let element of elements)
@@ -42,42 +40,38 @@ router.post('/url', function(req, res, next) {
     await page.screenshot({
       path: `public/images/${req.body.uid}/tmp.jpg`,
       fullPage: true,
+      quality: 90,
     });
     await browser.close();
     await res.json({imageUrl: `http://informatube.ngrok.io/images/${req.body.uid}/tmp.jpg?dummy=`+time});
   })();
 })
 
-router.post('/', function(req, res, next) {
+router.post('/', async (req, res, next) => {
   //infostamp 추가
-  const infostamp = new Infostamp({
-    stamper: req.body.uid,
-    url: req.body.url,
-    info: req.body.info,
-    time: req.body.time,
-    scroll: req.body.scroll,
-    likedUsers: [],
-    unlikedUsers: [],
-  });
-  infostamp.save()
-    .then((result) => {
-      const imgDir = `public/images/${req.body.uid}`;
-      base64Img.img(req.body.imgData,imgDir,'tmp_sketch',(err, filepath) => {
-        const dimension = sizeOf(`public/images/${req.body.uid}/tmp.jpg`);
-        sharp.cache(false);
-        console.log(filepath);
-        sharp(filepath)
-          .resize({width: dimension.width})
-          .composite([{input:`${imgDir}+/tmp.jpg'`, blend:'overlay'}])
-          .toFile(`${imgDir}/${result.id}.jpg`);
-      })
-      return Infostamp.populate(result, {path: 'stamper'});
-    })
-    .then((result) => {
-      console.log('save');
-      res.status(201).json(result);
-    })
-  
+  try{
+    const infostamp = new Infostamp({
+      stamper: req.body.uid,
+      url: req.body.url,
+      info: req.body.info,
+      time: req.body.time,
+      scroll: req.body.scroll,
+      likedUsers: [],
+      unlikedUsers: [],
+    });
+    const imgDir = `public/images/${req.body.uid}`;
+    console.log(imgDir);
+    const result = await infostamp.save();
+    const filepath = await base64Img.imgSync(req.body.imgData, imgDir, 'tmp_sketch');
+    await sharp.cache(false);
+    const composition = await sharp(filepath).resize({width: 399})
+      .composite([{input:`${imgDir}/tmp.jpg`, blend:'overlay'}])
+    await composition.jpeg({quality: 90}).toFile(`${imgDir}/${result.id}.jpg`);
+    const savedInfostamp = await Infostamp.populate(result, {path: 'stamper'});
+    res.status(201).json(savedInfostamp);
+  } catch(e) {
+    console.error(e);
+  }
 })
 
 module.exports = router;
